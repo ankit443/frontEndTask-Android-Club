d3ab0f05e185b82fb1b7f791c43a2b77cf06f93c
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-10 12:32:22 +0530

    working on front end design

---
+++ app/src/main/java/com/androidclub/frontendspirit/MainActivity.java
@@ -0,0 +1,23 @@
+package com.androidclub.frontendspirit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnSignUpMain, btnLoginMain;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
