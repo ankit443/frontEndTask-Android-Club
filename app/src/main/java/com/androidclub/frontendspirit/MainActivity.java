7a98bebd2b0c784505fb07d42e2569cdbd6f0c4f
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-10 13:05:09 +0530

    working on dashboard

--- app/src/main/java/com/androidclub/frontendspirit/MainActivity.java
+++ app/src/main/java/com/androidclub/frontendspirit/MainActivity.java
@@ -2,7 +2,9 @@ package com.androidclub.frontendspirit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,12 +12,33 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnSignUpMain, btnLoginMain;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignUpMain = findViewById(R.id.btnSignUpMain);
+        btnLoginMain = findViewById(R.id.btnLoginMain);
+
+        btnSignUpMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, SignUpPage.class);
+                startActivity(i);
+
+            }
+        });
+
+        btnLoginMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent k = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(k);
+
+            }
+        });
 
 
 
